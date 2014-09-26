ec054311025ccb1e75e53bf02e569f5632de2d3b
David Link <dlink@gmail.com>
2014-09-26 01:44:38 +0000

    Examples: Books: added isbn

--- examples/books/sql/create_books.sql
+++ examples/books/sql/create_books.sql
@@ -4,8 +4,10 @@ drop table /*! if exists */ books;
 
 create table books (
    id                int unsigned not null primary key,
+   isbn              varchar(13),
    name              varchar(255),
-   published         int unsigned comment 'Publication year'
+   published         int unsigned comment 'Publication year',
+   classification    enum('Novel', 'Nonfiction', 'Play', 'Poetry')
 ) 
 engine InnoDB default charset=utf8;
 ;
