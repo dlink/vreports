c4fa5ace38576c9be6034d0654237e321223f273
David Link <dlink@napower.com>
2014-10-10 17:19:01 -0400

    Can now generate Columns.yaml from database tables

---
+++ bin/genColumnsYaml.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import re
+import os
+import sys
+
+from vlib import db
+from vlib.datatable import DataTable
+from vlib.utils import pretty
+
+class Generator(object): 
+    def __init__(self):
+        self.db = db.getInstance()
+
+    def genYaml(self, tablename, alias):
+        '''Given a tablename and an given alias
+           Read table schema info from the database
+           Return vreports Column yaml file
+        '''
+        # Read columns info from database
+        dt = DataTable(self.db, tablename)
+        columns = []
+        for row in dt.describe():
+            name = row['Field']
+            db_type = row['Type']
+
+            # Convert DB types to vreports Types:
+            #   remove numbers from db_type
+            db_type = re.sub(r'[0-9]', r'', db_type)
+            if db_type in ('int()', 'tinyint()'):
+                type = 'integer'
+            elif db_type in ('char()', 'varchar()'):
+                type = 'string'
+            elif db_type in ('datetime'):
+                type = 'datetime'
+            elif db_type in ('decimal(,)'):
+                type = 'money'
+            else:
+                raise Exception('Unrecognized db_type: %s' % db_type)
+            columns.append({'name': name, 'type': type})
+
+        # Create Yaml by hand
+        #   not using yaml.dump for more control
+        o = 'columns:\n\n'
+        for i, c in enumerate(columns):
+            o += '   - name   : %s\n' % c['name']
+            o += '     select : %s.%s\n' % (alias, c['name'])
+            o += '     type   : %s\n' % c['type']
+
+            # set the first three columns to default: true
+            if i in range(3):
+                o += '     default: true\n'
+            o += '\n'
+        return o
+
+def syntax():
+    progname = os.path.basename(sys.argv[0])
+    print "%s <table_name> <table_alias>" % progname
+    print
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        syntax()
+
+    table_name  = sys.argv[1]
+    table_alias = sys.argv[2]
+
+    g=Generator()
+    print g.genYaml(table_name, table_alias)
