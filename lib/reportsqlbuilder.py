9bb4e5a624ac5558a73b2694adcd0a03d83520fe
David Link <dvlink@gmail.com>
2026-02-24 22:33:53 -0500

    ReportSQLBuilder: support for multi_value_triggers and no_summary_if

--- lib/reportsqlbuilder.py
+++ lib/reportsqlbuilder.py
@@ -164,9 +164,20 @@ class ReportSqlBuilder(object):
         return ', '.join(select)
 
     def _getTotalsSelectClause(self):
+        # get multi_value_triggers
+        multi_value_triggers = []
+        for control in self.params.controls:
+            if control.get('multi_value_trigger') and \
+               control.value and \
+               control.type == 'multi_menu' and \
+               len(control.value) > 1:
+                multi_value_triggers.append(control.multi_value_trigger)
+
         select = []
         for c in self.reportColumns.getSelectedColumns():
             if c.get('no_summary_totals'):
+                s = "''"
+            elif c.get('no_summary_if') in multi_value_triggers:
                 s = "''"
             elif self.params.get('group_bys') \
                and isSumOrCount(c.get('aggregate_func')):
